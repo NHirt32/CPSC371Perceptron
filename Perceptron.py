e5f15e838cc1581c3624c0163f22697f4b2f7136
ahunterow <tiredofalreadyusednames@gmail.com>
2023-03-02 14:39:38 -0800

    Updating the weights, concept of epochs, but no updating the bias yet. Not fully tested,

--- Perceptron.py
+++ Perceptron.py
@@ -1,9 +1,12 @@
 
 class Perceptron:
-    """Core object of the perceptron, has one useful method predict()."""
-    def __init__(self, weights, bias):
+    """Core object of the perceptron."""
+    def __init__(self, weights, bias, lrate, data, iterations):
         self.weights = weights
         self.bias = bias
+        self.lrate = lrate
+        self.data = data
+        self.iterations = iterations
 
     def sign(self, val):
         """Optimistically says that the 0 value will map to 1."""
@@ -13,9 +16,56 @@ class Perceptron:
             return 1
 
     def predict(self, attributes):
-        """Takes a list of numeric attributes representing a mushroom, returns whether they are poisonous or not."""
+        """Takes a list of numeric attributes representing a mushroom, returns whether they are poisonous or not.
+            Does not take the output value, so remember to pass in appropriate slices."""
         result = 0
         for index, attribute in enumerate(attributes):
             result = result + ((self.weights[index] * attribute) + self.bias)
 
         return self.sign(result)
+
+    def train_datapoint(self, datapoint):
+        """Takes in a datapoint, adjusts weights accordingly."""
+        attributes = datapoint[slice(1, len(datapoint))]
+        tested_result = self.predict(attributes)
+
+        true_result = 1
+
+        # if is poisonous
+        if datapoint[0] == 15:
+            true_result = -1
+
+        err = true_result - tested_result
+
+        if err != 0:
+            for index, weight in enumerate(self.weights):
+                self.weights[index] = weight + (self.lrate * (true_result - tested_result) * attributes[index])
+            return 1
+        else:
+            return 0
+
+    def train(self):
+        """Takes in a bunch of datapoints, and adjusts the weights accordingly. Iterates
+            for as many times as is set in the iterations field."""
+        counter = 0
+        errors = 0
+
+        while counter < self.iterations:
+            for datapoint in self.data:
+                errors = errors + self.train_datapoint(datapoint)
+
+            print(self.to_string() +"\nNumber of errors thus far: " + str(errors))
+            counter = counter + 1
+
+
+
+    def to_string(self):
+        String = "weights: "
+        for weight in self.weights:
+            String = String + str(weight) + " "
+
+        String = String + "\nbias: " + str(self.bias)
+
+        String = String + "\nlearning rate: " + str(self.lrate)
+
+        return String
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+"""Runnable for the mushroom perceptron"""
+
 import Parsers
-import Perceptron
+from Perceptron import *
 
 file = "MushroomData_8000.txt"
 
@@ -7,3 +9,10 @@ mushrooms = Parsers.file_read(file)
 
 for mushroom in mushrooms:
     print(mushroom)
+
+weights = []
+for attribute in mushrooms[0][slice(1,len(mushrooms[0]))]:
+    weights.append(0)
+
+mushyfinder = Perceptron(weights, 1, 0.1, mushrooms, 1000)
+mushyfinder.train()
