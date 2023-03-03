cd3a0e13a2e4e46d9399f6669064afa3c7728069
ahunterow <tiredofalreadyusednames@gmail.com>
2023-03-02 16:08:56 -0800

    Using learning rate 0.1, initial bias and weights 0, trained up to epoch 10009

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ weights = []
 for attribute in mushrooms[0][slice(1, len(mushrooms[0]))]:
     weights.append(0)
 
-mushroom_perceptron = Perceptron(weights, 0, 0.1, mushrooms, 10, 0, 1)
+mushroom_perceptron = Perceptron(weights, 0, 0.1, mushrooms, 10000, 0, 1)
 mushroom_perceptron.load("Perceptron_info.json")
 mushroom_perceptron.train()
 mushroom_perceptron.save("Perceptron_info.json")
